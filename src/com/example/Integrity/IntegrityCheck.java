294d620263d6544324e579d4fb56364ed901c5db
moqiliangyu <10212878@bupt.edu.cn>
2016-04-04 09:29:23 +0800

    v1.0

--- src/com/example/Integrity/IntegrityCheck.java
+++ src/com/example/Integrity/IntegrityCheck.java
@@ -2,6 +2,7 @@ package com.example.Integrity;
 
 import android.util.Log;
 import android.util.SparseArray;
+import android.util.SparseIntArray;
 
 import com.example.celluar.CellularDown;
 import com.example.entities.FileFragment;
@@ -11,7 +12,7 @@ public class IntegrityCheck {
 	// single instance mode
 	private static IntegrityCheck instance;
 	private SparseArray<Segment> urlMap;
-	private SparseArray<String> idURIMap;
+	private SparseArray<Integer> idURIMap;
 	private static int idCount = -1;
 	public static final String URL_TAG = "http://buptant.cn/autoChart/du/video/ljw2016/zxyqwe/download.php";
 	public static final String URI_TAG = "http://127.1.1.1:9999/";
@@ -21,14 +22,14 @@ public class IntegrityCheck {
 
 	private IntegrityCheck() {
 		urlMap = new SparseArray<Segment>();
-		idURIMap = new SparseArray<String>();
+		idURIMap = new SparseArray<Integer>();
 		insertURLtoMaps(idURIMap);
 		celluDown = new CellularDown();
 	}
 
-	private void insertURLtoMaps(SparseArray<String> idUri) {
+	private void insertURLtoMaps(SparseArray<Integer> idUri) {
 		for (int i = 1; i <= TOTAL_SEGS; i++) {
-			idUri.put(++idCount, URI_TAG + i + ".mp4");
+			idUri.put(++idCount, Integer.valueOf(i));//URI_TAG + i + ".mp4");
 			// Log.v("map", idUrl.get(0));
 		}
 
@@ -76,19 +77,30 @@ public class IntegrityCheck {
 	private int uri2id(String uri) {
 		// TODO Auto-generated method stub
 		synchronized (this) {
+			int count = getIndex(uri);
 			boolean t = false;
 			int i = 0;
 			while (!t) {
 				if (i == TOTAL_SEGS) {
 					return -2;
 				}
-				t = idURIMap.get(i).equals(uri);
+				t = idURIMap.get(i).intValue()==count;
 				i++;
 
 			}
 			// return idURIMap.indexOfValue(uri);
 			return (--i);
 		}
+	}
+	private int getIndex(String uri){
+		String fileName = "";
+		for (String s : uri.split("/")) {
+			if (s.contains(".mp4")) {
+				fileName = s;
+				break;
+			}			
+		}		
+		return Integer.parseInt(fileName.substring(0, 1));
 	}
 
 	public void setSegLength(int id, int totalLength) {
