6f14e9f7bc51e763d53ca4f47cd9f1794d0063d0
zxyqwe <zxyqwe_2004@126.com>
2016-04-05 10:05:50 +0800

    3g more

---
+++ src/com/example/celluar/CellularMore.java
@@ -0,0 +1,34 @@
+package com.example.celluar;
+
+import android.util.Log;
+
+import com.example.Integrity.IntegrityCheck;
+import com.example.entities.Segment;
+
+public class CellularMore extends Thread {
+	private static final String TAG = CellularMore.class.getSimpleName();
+
+	private int url;
+
+	public CellularMore(int url) {
+		this.url = url;
+	}
+
+	@Override
+	public void run() {
+		IntegrityCheck IC = IntegrityCheck.getInstance();
+		Segment Seg = IC.getSeg(url);
+		if (Seg != null) {
+			while (!Seg.checkIntegrity()) {
+				Log.d(TAG, "no");
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+				}
+			}
+			Log.d(TAG, "yes");
+		} else {
+			Log.e(TAG, "a");
+		}
+	}
+}
