dd82385996842e11d6ba79dd78bb7ec28c504e21
zxyqwe <zxyqwe_2004@126.com>
2016-05-03 16:16:24 +0800

    异常调试

--- src/com/ANT/MiddleWare/DASHProxyServer/DashProxyServer.java
+++ src/com/ANT/MiddleWare/DASHProxyServer/DashProxyServer.java
@@ -38,59 +38,42 @@ public class DashProxyServer extends NanoHTTPD {
 	@Override
 	public Response serve(IHTTPSession session) {
 		Log.v(TAG, "filename " + session.getUri());
-		try {
-			if (!getFileName(session, ".m3u8").equals("")) {
-				return localFile("index.m3u8");
-			}
-			String playist = getFileName(session, ".mp4");
-			Log.v(TAG, "playist" + playist);
-			if (!playist.equals("")) {
-				Log.v(TAG, "mp4");
-				switch (MainFragment.configureData.getWorkingMode()) {
-				case LOCAL_MODE:
-					return localFile(playist);
-				case G_MDOE:
-					IntegrityCheck iTC = IntegrityCheck.getInstance();
-					int tmpp = Integer.parseInt(playist.substring(0, 1));
-					byte[] tmp = iTC.getSegments(tmpp);
-					return newFixedLengthResponse(Response.Status.OK, mp4, tmp);
-				default:
-					Log.wtf(TAG, "mode wrong");
-					return newFixedLengthResponse(
-							Response.Status.INTERNAL_ERROR, mp4, null, 0);
-				}
-			} else if (MainFragment.configureData.getWorkingMode() == WorkMode.JUNIT_TEST_MODE) {
-				Log.v(TAG, "junit");
-				Stack<FileFragment> s = CellularDownTest.fraList;
-				if (s.empty()) {
-					Log.wtf(TAG, "stack empty");
-					return newFixedLengthResponse(Response.Status.OK,
-							mp4, null, 0);
-				}
-				FileFragment f = s.pop();
-				Response res = newFixedLengthResponse(
-						Response.Status.PARTIAL_CONTENT, mp4, f.getData());
-				res.addHeader(
-						"Content-Range",
-						"Content-Range " + f.getStartIndex() + "-"
-								+ f.getStopIndex() + "/"
-								+ CellularDownTest.base);
-				return res;
-			}
-			Log.wtf(TAG, "file nothing");
-			return newFixedLengthResponse(Response.Status.NOT_FOUND, mp4, null,
-					0);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			Log.wtf(TAG, e);
-			return newFixedLengthResponse(Response.Status.NOT_FOUND, mp4, null,
-					0);
-		} catch (IOException e) {
-			e.printStackTrace();
-			Log.wtf(TAG, e);
-			return newFixedLengthResponse(Response.Status.BAD_REQUEST, mp4,
-					null, 0);
+		if (!getFileName(session, ".m3u8").equals("")) {
+			return localFile("index.m3u8");
 		}
+		String playist = getFileName(session, ".mp4");
+		Log.v(TAG, "playist" + playist);
+		if (!playist.equals("")) {
+			Log.v(TAG, "mp4");
+			switch (MainFragment.configureData.getWorkingMode()) {
+			case LOCAL_MODE:
+				return localFile(playist);
+			case G_MDOE:
+				IntegrityCheck iTC = IntegrityCheck.getInstance();
+				int tmpp = Integer.parseInt(playist.substring(0, 1));
+				byte[] tmp = iTC.getSegments(tmpp);
+				return newFixedLengthResponse(Response.Status.OK, mp4, tmp);
+			default:
+				Log.wtf(TAG, "mode wrong");
+				return newFixedLengthResponse(Response.Status.INTERNAL_ERROR,
+						mp4, null, 0);
+			}
+		} else if (MainFragment.configureData.getWorkingMode() == WorkMode.JUNIT_TEST_MODE) {
+			Log.v(TAG, "junit");
+			Stack<FileFragment> s = CellularDownTest.fraList;
+			if (s.empty()) {
+				Log.w(TAG, "stack empty");
+				return newFixedLengthResponse(Response.Status.OK, mp4, null, 0);
+			}
+			FileFragment f = s.pop();
+			Response res = newFixedLengthResponse(
+					Response.Status.PARTIAL_CONTENT, mp4, f.getData());
+			res.addHeader("Content-Range", "Content-Range " + f.getStartIndex()
+					+ "-" + f.getStopIndex() + "/" + CellularDownTest.base);
+			return res;
+		}
+		Log.wtf(TAG, "file nothing");
+		return newFixedLengthResponse(Response.Status.NOT_FOUND, mp4, null, 0);
 	}
 
 	private String getFileName(IHTTPSession session, String key) {
@@ -104,11 +87,21 @@ public class DashProxyServer extends NanoHTTPD {
 		return playlist;
 	}
 
-	private Response localFile(String str) throws IOException {
-		str = dir + str;
-		FileInputStream fis = new FileInputStream(
-				Environment.getExternalStorageDirectory() + str);
-		int length = fis.available();
+	private Response localFile(String str) {
+		str = Environment.getExternalStorageDirectory() + dir + str;
+		Log.e(TAG, str);
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(str);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		int length = 0;
+		try {
+			length = fis.available();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return newFixedLengthResponse(Response.Status.OK, mp4, fis, length);
 	}
 
